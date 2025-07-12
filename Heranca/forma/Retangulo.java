25f8cb4035a5a85f7669ee0bbb2ae8de84cad279
Felipe Anias <felipeanias@hotmail.com>
2025-07-12 19:16:05 -0300

    licao 4 - retangulo

---
+++ Heranca/forma/Retangulo.java
@@ -0,0 +1,40 @@
+
+import java.text.DecimalFormat;
+
+
+
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if(largura <= 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }else{
+            DecimalFormat df = new DecimalFormat("#.##");
+            this.largura = Double.parseDouble(df.format(largura).replace(",", "."));
+        }
+        
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if(altura <= 0) {
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }else{
+            DecimalFormat df = new DecimalFormat("#.##");
+            this.altura = Double.parseDouble(df.format(altura).replace(",", "."));
+        }
+    }
+
+
+
+    
+}
