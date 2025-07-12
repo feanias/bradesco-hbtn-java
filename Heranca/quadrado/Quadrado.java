5dd3d00297af9993b962c6b8e952806624efe4be
Felipe Anias <felipeanias@hotmail.com>
2025-07-12 19:43:19 -0300

    licao 5

---
+++ Heranca/quadrado/Quadrado.java
@@ -0,0 +1,26 @@
+import java.text.DecimalFormat;
+
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if(lado <= 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+            DecimalFormat df = new DecimalFormat("#.##");
+            this.lado = Double.parseDouble(df.format(lado).replace(",", "."));
+            setLargura(this.lado);
+            setAltura(this.lado);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().format("[Quadrado] %.2f", lado);
+    }
+    
+}
