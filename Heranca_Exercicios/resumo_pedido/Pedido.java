72f9546ed311e9d3ad450e9a4b9132b87897425c
Felipe Anias <felipeanias@hotmail.com>
2025-07-13 20:08:31 -0300

    tentativa correcao

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -1,3 +1,5 @@
+
+
 public class Pedido {
     private double percentualDesconto;
     private ItemPedido[] itens;
@@ -7,53 +9,39 @@ public class Pedido {
         this.itens = itens;
     }
 
-    public void calcularTotalDesconto() {
-        double totalDesconto = 0.0;
-        for (ItemPedido item : itens) {
-            totalDesconto += item.calcularTotal() * (percentualDesconto / 100);
-        }
-        this.percentualDesconto = totalDesconto;
-    }
-    
-    public double calcularTotalBruto() {
+    public double calcularTotal() {
         double total = 0.0;
         for (ItemPedido item : itens) {
-            total += item.getQuantidade() * (item.getProduto().getPrecoBruto());
+            if(this.percentualDesconto > 0){
+                total += item.getQuantidade() * (item.getProduto().obterPrecoLiquido() - (item.getProduto().obterPrecoLiquido() * (percentualDesconto / 100)));
+            } else {
+                total += item.getQuantidade() * item.getProduto().obterPrecoLiquido();
+            }
         }
         return total;
     }
 
-     public double calcularTotalComDesconto() {
-        double total = 0.0;
-        for (ItemPedido item : itens) {
-            total += item.getQuantidade() * item.getProduto().obterPrecoLiquido() 
-                    * (1 - (percentualDesconto / 100));
-        }
-        return total;
-    }
 
     public void apresentarResumoPedido() {
-        double valorDesconto = 0.0;
-        double total = 0.0;
-        System.out.println("------- RESUMO PEDIDO ------");
-        for (ItemPedido item : itens) {
-            System.out.printf("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",
-                    item.getProduto().getClass().getSimpleName(),
-                    item.getProduto().getTitulo(),
-                    item.getProduto().getPrecoBruto(),
-                    item.getQuantidade(),
-                    item.calcularTotal()
-                    );
-            total+= item.calcularTotal();
-
-            valorDesconto += item.calcularTotalDesconto(percentualDesconto);
-        }   
+        double descontoItem = 0.0;
+        System.out.println("------- RESUMO PEDIDO -------");
+        for(ItemPedido item: itens) {
+            descontoItem += ((item.getQuantidade())* item.getProduto().obterPrecoLiquido()) * (percentualDesconto /100);
+            System.out.printf("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f \n", 
+                item.getProduto().getClass().getSimpleName(), 
+                item.getProduto().getTitulo(), 
+                item.getProduto().obterPrecoLiquido(), 
+                item.getQuantidade(), 
+                item.obterPrecoLiquido() * item.getQuantidade());
+                
+        }
         System.out.println("----------------------------");
-        System.out.printf("DESCONTO: %.2f\n", valorDesconto);
-        System.out.printf("TOTAL PRODUTOS: %.2f\n", total);
+        System.out.printf("DESCONTO: %.2f \n", descontoItem);
+        System.out.printf("TOTAL PRODUTOS: %.2f \n", calcularTotal() + descontoItem);
         System.out.println("----------------------------");
-        System.out.printf(  "TOTAL PEDIDO: %.2f\n", calcularTotalComDesconto());
-        System.out.println("---------------------------- ");
+        System.out.printf("TOTAL PEDIDO: %.2f \n", calcularTotal());
     }
-        
+
+
+  
 }
