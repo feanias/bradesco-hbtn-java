05d5415da6c846b0e24f8c6406992b981e60ab6f
Felipe Anias <felipeanias@hotmail.com>
2025-07-14 08:38:31 -0300

    alteracao

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -39,7 +39,7 @@ public class Pedido {
         System.out.printf("DESCONTO: %.2f\n", descontoItem);
         System.out.printf("TOTAL PRODUTOS: %.2f\n", calcularTotal() + descontoItem);
         System.out.println("----------------------------");
-        System.out.printf("TOTAL PEDIDO: %.2f \n", calcularTotal());
+        System.out.printf("TOTAL PEDIDO: %.2f\n", calcularTotal());
         System.out.println("----------------------------");
     }
 
