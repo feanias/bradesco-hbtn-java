70dd390ea42a7a7a57e7b53324a5307b1c9793fb
Felipe Anias <felipeanias@hotmail.com>
2025-07-19 10:25:18 -0300

    adicionando lógica da aplicação.

--- 0x02/quadrado/Program.java
+++ 0x02/quadrado/Program.java
@@ -3,8 +3,20 @@ package quadrado;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+            double lado1 = -5;
+            double resultado = Quadrado.area(lado1);
+            System.out.println(resultado);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
+        try {
+            double lado2 = 3;
+            double resultado = Quadrado.area(lado2);
+            System.out.println(resultado);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 	}
-
 }
--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,5 +1,13 @@
 package quadrado;
 
 public class Quadrado {
+	public static double area(double lado) throws IllegalArgumentException {
+		String mensagem = "Lado deve possuir valor positivo";
+		if(lado <= 0) {
+			throw new IllegalArgumentException(mensagem);
+		} else {
+			return lado * lado;
+		}		
+	}
 
 }
