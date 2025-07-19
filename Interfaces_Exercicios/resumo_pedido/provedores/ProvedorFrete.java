bc5ce6d4ebedabbd032b0790fa892d9d1ad8f71d
Felipe Anias <felipeanias@hotmail.com>
2025-07-18 22:04:46 -0300

    adequacao
    adequacao

--- Interfaces_Exercicios/resumo_pedido/provedores/ProvedorFrete.java
+++
@@ -1,8 +0,0 @@
-package provedores;
-
-public interface ProvedorFrete {
-    
-    public Frete calcularFrete(double peso, double valor);
-
-    public TipoProvedorFrete obterTipoProvedorFrete(TipoProvedorFrete frete);
-}
