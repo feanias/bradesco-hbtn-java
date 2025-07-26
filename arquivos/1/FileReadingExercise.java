f9cd880502bcc2cc639e1bdf2935ab9172692d50
Felipe Anias <felipeanias@hotmail.com>
2025-07-26 12:54:28 -0300

    atualização
    atualização exercicio 1

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExercise {
+    public static void main(String[] args) throws IOException {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+        fileName = "arquivos/1/" + fileName; // Caminho do arquivo
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            
+            String line = null;
+            line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+            System.out.println("");
+            // efetue a leitura do arquivo
+        } catch (IOException ex) {
+            System.out.println("Erro ao ler o arquivo: " + ex.getMessage());
+        } finally {
+            // Aqui você pode fechar o BufferedReader se necessário
+            System.out.println("Leitura do arquivo concluída.");
+            br.close();
+        }
+
+    }
+}
