d1519fa0c07c82d93df0c650e95876bdd4cffbbd
Felipe Anias <felipeanias@hotmail.com>
2025-07-26 14:17:35 -0300

    Update FileReadingExercise.java

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -15,11 +15,14 @@ public class FileReadingExercise {
                 System.out.println(line);
                 line = br.readLine();
             }
+            System.out.println("");
+            
         } catch (IOException ex) {
             System.out.println("Erro ao ler o arquivo: " + ex.getMessage());
         } finally {
             // Aqui você pode fechar o BufferedReader se necessário
             System.out.println("Leitura do arquivo concluída.");
+            System.out.println(" ");
             br.close();
         }
 
