e9a30560081c87fd8b50a6e11f80a8a57ff92328
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-07-26 20:09:07 -0300

    Update FileReadingExercise.java

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
+
 
 public class FileReadingExercise {
     public static void main(String[] args) {
