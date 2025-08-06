f0f23d97e3812800915c8cbdfb6c1bdf98e7e7e5
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-08-06 11:16:04 -0300

    Exercicio 2

---
+++ complexidade/2/PessoasArray.java
@@ -0,0 +1,25 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de buscaBinaria
+    public void buscaBinaria(String nome){
+        boolean encontrado = false;
+
+    }
+}
