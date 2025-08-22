be6dbcdd18e913dddce88d140c4d53309d895a1a
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-08-21 21:13:08 -0300

    Exercicio 1 - Cookies

--- List/cookies/Pedido.java
+++ List/cookies/Pedido.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Pedido {
     static private ArrayList<PedidoCookie> cookie;
@@ -17,6 +18,19 @@ public class Pedido {
             totalCaixasPedidas+= pedidoCookie.getQuantidadeCaixas();            
         }
         return totalCaixasPedidas;
+    }
+
+    public int removerSabor(String sabor){
+        int totalCaixasRemovidas = 0;
+        Iterator<PedidoCookie> iterador = cookie.iterator();
+        while (iterador.hasNext()) {
+            PedidoCookie item = iterador.next();
+                if (item.getSabor() == sabor) {
+                    totalCaixasRemovidas += item.getQuantidadeCaixas();
+                    iterador.remove();
+                }
+        }    
+        return totalCaixasRemovidas;
     }
 
 }
--- List/cookies/Program.java
+++ List/cookies/Program.java
@@ -1,15 +1,36 @@
 public class Program {
+        public static void main(String[] args) {
+            Pedido pedido = new Pedido();
 
+            pedido.adicionarPedidoCookie(new PedidoCookie("Chocolate", 10));
+            pedido.adicionarPedidoCookie(new PedidoCookie("Nutella", 3));
+            pedido.adicionarPedidoCookie(new PedidoCookie("Baunilha", 2));
 
-    public static void main(String[] args) {
-    Pedido pedido = new Pedido();
+            System.out.println(String.format("Total: %d", pedido.obterTotalCaixas()));
 
-    pedido.adicionarPedidoCookie(new PedidoCookie("Chocolate", 10));
-    pedido.adicionarPedidoCookie(new PedidoCookie("Nutella", 3));
-    pedido.adicionarPedidoCookie(new PedidoCookie("Baunilha", 2));
+            pedido.adicionarPedidoCookie(new PedidoCookie("Chocolate", 1));
+            pedido.adicionarPedidoCookie(new PedidoCookie("Nutella", 1));
 
-    pedido.obterTotalCaixas();
+            System.out.println(String.format("Total: %d", pedido.obterTotalCaixas()));
 
-    }   
-    
-}   
+            int quantidadeRemovidos = pedido.removerSabor("Nutella");
+
+            System.out.println(String.format("Total: %d - Removidos: %d", pedido.obterTotalCaixas(), quantidadeRemovidos));
+
+            quantidadeRemovidos = pedido.removerSabor("Chocolate");
+
+            System.out.println(String.format("Total: %d - Removidos: %d", pedido.obterTotalCaixas(), quantidadeRemovidos));
+
+            quantidadeRemovidos = pedido.removerSabor("Baunilha");
+
+            System.out.println(String.format("Total: %d - Removidos: %d", pedido.obterTotalCaixas(), quantidadeRemovidos));
+
+            quantidadeRemovidos = pedido.removerSabor("Chocolate");
+
+            System.out.println(String.format("Total: %d - Removidos: %d", pedido.obterTotalCaixas(), quantidadeRemovidos));
+
+            pedido.adicionarPedidoCookie(new PedidoCookie("Nutella", 5));
+
+            System.out.println(String.format("Total: %d", pedido.obterTotalCaixas()));
+        }
+    }
