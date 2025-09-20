0fe301185780ae7670a4e3a63200a18e92beeafa
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-09-20 12:26:25 -0300

    Exercicio 04

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Cliente.java
@@ -0,0 +1,34 @@
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private boolean premium;
+
+    public Cliente(int codigo, String nome, Boolean premium){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.premium = premium;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setPremium(boolean premium) {
+        this.premium = premium;
+    }
+}
