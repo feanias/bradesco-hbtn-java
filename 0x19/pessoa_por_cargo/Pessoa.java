d40c66ccb940e915494bfd94989c2f3f28b9e764
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-09-25 20:05:38 -0300

    Exercicio 00

--- 0x19/pessoa_por_cargo/Pessoa.java
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Pessoa {
     
     private int codigo;
@@ -32,7 +34,7 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return String.format("[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
+        return String.format(new java.util.Locale("pt", "BR"),"[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
     }
 
     
